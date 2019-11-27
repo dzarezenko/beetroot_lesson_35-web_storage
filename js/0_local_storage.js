aee4b5b7c7cabdf0d6aa04e57255eb08007ea288
Dmytro Zarezenko <dmytro.zarezenko@gmail.com>
2019-11-27 19:17:08 +0200

    Local storage example

---
+++ js/0_local_storage.js
@@ -0,0 +1,9 @@
+window.localStorage.setItem('name', "Jone Dou");
+
+let name = window.localStorage.getItem('name');
+console.log(name);
+
+window.localStorage.removeItem('name');
+window.localStorage.clear();
+
+// window.sessionStorage
